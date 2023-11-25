4c09ca66c40bf16676ddf9b8f5f6ee576e48d1c5
eren0049 <eren.baysal1@gmail.com>
2023-11-25 08:56:26 +0100

    * my new version

--- app/src/main/java/com/example/exploergithub/MainActivity.java
+++ app/src/main/java/com/example/exploergithub/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        inItViews();
         setContentView(R.layout.activity_main);
     }
 
